63636d036f51f8d99c77d5618240f9abfcc27903
Chris <chris@fourcore-computing.co.uk>
2018-08-05 00:34:27 +0100

    Missed file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <unistd.h>
 
 int main()
 {
 	initscr();
+
+	move(3,3);
+	addstr("Testing...");
+	refresh();
+
+	sleep(1);
 
 	endwin();
 
